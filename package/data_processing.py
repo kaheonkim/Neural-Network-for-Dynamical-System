7b3ce25f652c9d3441ffd943902d433c3463b634
kaheonkim <kdkam12@unist.ac.kr>
2023-05-14 16:04:11 +0900

    Delete data_processing.py

--- package/data_processing.py
+++
@@ -1,9 +0,0 @@
-def input_data(seq,ws):
-    out = []
-    L = len(seq)
-    
-    for i in range(L-ws):
-        window = seq[i:i+ws]
-        label = seq[i+ws:i+ws+1]
-        out.append((window,label))
-    return out
